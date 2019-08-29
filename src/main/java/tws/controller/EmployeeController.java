bc5c4736a156ef6101713d89f4b39f5334108c5d
wenmingwy <2377812120@qq.com>
2019-08-29 20:54:38 +0800

    wm

--- src/main/java/tws/controller/EmployeeController.java
+++ src/main/java/tws/controller/EmployeeController.java
@@ -1,19 +1,24 @@
 package tws.controller;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
 import tws.entity.Employee;
 import tws.repository.EmployeeMapper;
-
-import java.net.URI;
-import java.util.List;
-import java.util.UUID;
-
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
@@ -25,5 +30,24 @@ public class EmployeeController {
     public ResponseEntity<List<Employee>> getAll() {
         return ResponseEntity.ok(employeeMapper.selectAll());
     }
+
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Employee createEmployee(@RequestBody Employee employee){
+        employeeMapper.insertOne(employee);
+        return employee;
+    }
+
+	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public void updateEmployee(@PathVariable int id, @RequestBody Employee employee) {
+		employeeMapper.updateOne(id, employee);
+	}
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public void deleteEmployee(@PathVariable int id) {
+		employeeMapper.deleteOne(id);
+	}
 
 }
